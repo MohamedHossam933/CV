b3f68ea7f8c98040b172743953e87fc70ff4acd4
MohamedHossam933 <mohamed.hossam7799@gmail.com>
2019-09-16 22:38:42 +0200

    initial commit

--- Tasks/Websites/facebook/facebook.js
+++ Tasks/Websites/facebook/facebook.js
@@ -1,15 +1,31 @@
-var objPeople = [ {username: "admin",password: "admin"} , {username: "Admin",password: "Admin"} , {username: "ADMIN",password: "ADMIN"} ]
+//log in function to line 32
+var objPeople = [
+	{ // Object @ 0 index
+		username: "admin",
+		password: "admin"
+	},
+	{ // Object @ 1 index
+		username: "Admin",
+		password: "Admin"
+	},
+	{ // Object @ 2 index
+		username: "ADMIN",
+		password: "ADMIN"
+	}
+
+]
+
 function LogIn() {
-	var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
 
 	for(var i = 0; i < objPeople.length; i++) {
 		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
             // console.log(username + " is logged in!!!")
-            window.location.href = "https://www.facebook.com/";
+            window.location.href = "https://www.facebook.com";
 			// stop the function if this is found to be true
-			return;
+			return
 		}
 	}
 	alert ("incorrect username or password")
